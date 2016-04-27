7a0eecf527d4f106c03e4c4abce7fbd247cf52b3
Damian Bocanegra <damian.bocanegra@autodesk.com>
2016-04-27 13:13:29 -0500

    cambios

--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from 'angular2/core';
+import { ROUTER_DIRECTIVES } from 'angular2/router';
+
 import { IProduct } from './product';
 import { ProductFilterPipe } from './product-filter.pipe';
 import { StartComponent } from '../shared/star.component';
 import { ProductService } from './product.service';
 
 @Component({
-    selector: 'pm-products',
     templateUrl: 'app/products/product-list.component.html',
     styleUrls: ['app/products/product-list.component.css'],
     pipes: [ProductFilterPipe],
-    directives: [StartComponent]
+    directives: [StartComponent, ROUTER_DIRECTIVES]
 })
 export class ProductListComponent implements OnInit {
     pageTitle: string = 'Product List';
@@ -17,6 +18,7 @@ export class ProductListComponent implements OnInit {
     imageMargin: number = 2;
     showImage: boolean = false;
     listFilter: string;
+    errorMessage: string;
     products: IProduct[];
     
     constructor(private _productService: ProductService) {
@@ -28,7 +30,9 @@ export class ProductListComponent implements OnInit {
     }
     
     ngOnInit(): void {
-        this.products = this._productService.getProducts();
+        this._productService.getProducts()
+            .subscribe(products => this.products = products)
+            error => this.errorMessage = <any>error;
     }
     
     onRatingClicked(message: string): void {
